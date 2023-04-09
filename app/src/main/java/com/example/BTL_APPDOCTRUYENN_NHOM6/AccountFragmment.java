7cc838e3ae0141f8aff01d858c0bac15c67ff662
NguyenVanCong2051063627 <99166356+NguyenVanCong2051063627@users.noreply.github.com>
2023-04-10 00:10:05 +0700

    phanquyen+thongtin

---
+++ app/src/main/java/com/example/BTL_APPDOCTRUYENN_NHOM6/AccountFragmment.java
@@ -0,0 +1,48 @@
+package com.example.APPDOCTRUYEN_NHOM6;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.APPDOCTRUYEN_NHOM6.R;
+
+
+public class AccountFragmment extends Fragment {
+    private View view;
+    private TextView tvtaikhoan,tvgmail;
+    private Button btndangxuat;
+
+
+    public AccountFragmment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view =  inflater.inflate(R.layout.fragment_account_fragmment, container, false);
+
+        //ánh xạ
+        tvtaikhoan = view.findViewById(R.id.tvtentaikhoan);
+        tvgmail = view.findViewById(R.id.tvgmail);
+        btndangxuat = view.findViewById(R.id.btndangxuat);
+        tvtaikhoan.setText(MainActivity.tentaikhoan);
+        tvgmail.setText(MainActivity.email);
+
+        btndangxuat.setOnClickListener(view ->{
+            Intent intent = new Intent(getContext(),MainDangNhap.class);
+            startActivity(intent);
+        });
+        return view;
+    }
+}
